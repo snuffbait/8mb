b7dac7677f7a3661246b6339708f3043df8bdba8
Fae <139820940+snuffbait@users.noreply.github.com>
2026-04-07 20:45:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.matchAll().then(() => self.clients.claim())));
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).then(r => {
-    const headers = new Headers(r.headers);
-    headers.set('Cross-Origin-Opener-Policy', 'same-origin');
-    headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
-    return new Response(r.body, { status: r.status, statusText: r.statusText, headers });
-  }));
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const headers = new Headers(r.headers);
+      headers.set('Cross-Origin-Opener-Policy', 'same-origin');
+      headers.set('Cross-Origin-Embedder-Policy', 'credentialless');
+      return new Response(r.body, { status: r.status, statusText: r.statusText, headers });
+    }).catch(() => fetch(e.request))
+  );
 });

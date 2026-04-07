698620bd819f32b6bea0dea4f2f27a930288270c
Fae <139820940+snuffbait@users.noreply.github.com>
2026-04-07 21:00:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-//test
-
-
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', e => {
